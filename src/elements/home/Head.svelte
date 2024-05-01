596cd8aba53841681566d2f6b6f93631c2ddb27d
Koval Yaroslav <yaroslav@koval.page>
2024-05-01 17:03:52 +0200

    Update Home

---
+++ src/elements/home/Head.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    const goHome = (): void => {
+        window.open("/", "_self");
+    }
+</script>
+
+<div class="items-center text-center mt-40 md:text-[.5rem] md:mb-16 md:mt-48 md:text-lg text-base">
+    <div class="block">
+        <span class="text-black bg-white px-1 mt-2">
+            Поки що
+        </span>
+        <span>тут нічого немає</span>
+    </div>
+    <div class="block">
+        <span>There is nothing here</span>
+        <span class="text-black bg-white px-1 mt-2">
+            yet
+        </span>
+    </div>
+    <div class="block">
+        <span class="text-black bg-white px-1 mt-2">
+            Пока
+        </span>
+        <span>здесь ничего нет</span>
+    </div>
+</div>
