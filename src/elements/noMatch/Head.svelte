50c4ee8c3f4d272055a93785fdb2a7609ac4ff78
Koval Yaroslav <yaroslav@koval.page>
2024-05-01 16:52:56 +0200

    Update NoMatch

--- src/elements/noMatch/Head.svelte
+++ src/elements/noMatch/Head.svelte
@@ -1,4 +1,12 @@
+<script lang="ts">
+    const goHome = (): void => {
+        window.open("/", "_self");
+    }
+</script>
+
 <div class="flex gap-1 flex-col mb-12 items-center mt-40 md:text-[.5rem] md:mb-16 md:mt-48">
     <h1 class="font-extrabold text-4xl md:text-6xl">Nothing</h1>
-    <h2 class="text-base text-[#ebebf599] md:text-lg">there is really nothing on this page</h2>
+    <button class="text-base text-black bg-white px-1 mt-2 md:text-lg" on:click={goHome}>
+        there is really nothing on this page
+    </button>
 </div>
