a9f931cbf94d7c75197073a6863a547dff5e6800
Koval Yaroslav <yaroslav@koval.page>
2024-05-02 16:17:35 +0200

    Update Home

--- src/elements/home/Home.svelte
+++ src/elements/home/Home.svelte
@@ -1,17 +1,33 @@
 <script lang="ts">
+    import { Parallax, ParallaxLayer } from 'svelte-parallax';
+
     import Header from "../Header.svelte";
 
     import Stars from "../misc/Stars.svelte";
     import Head from "./Head.svelte";
+    import Coin from "./Coin.svelte";
+    import Text from "./Text.svelte";
+
+    let parallax: Parallax;
 </script>
 
 <Header />
 
 <div class="relative">
-    <div class="my-0 mx-auto py-0 px-4 max-w-[1040px]">
-        <div class="pb-16 w-full">
-            <Stars count={4} />
+    <Stars count={4} />
+
+    <Parallax sections={4} bind:this={parallax} config={{stiffness: 0.2, damping: 0.3}}>
+        <ParallaxLayer rate={0} offset={0}>
             <Head />
-        </div>
-    </div>
+        </ParallaxLayer>
+        <ParallaxLayer rate={-.45} offset={1} let:progress>
+            <Coin progress={progress} />
+        </ParallaxLayer>
+        <ParallaxLayer rate={.5} offset={1}>
+            <Text>3 months and 35 000 000 users later is was over</Text>
+        </ParallaxLayer>
+        <ParallaxLayer rate={.5} offset={2}>
+            <Text>or not</Text>
+        </ParallaxLayer>
+    </Parallax>
 </div>
