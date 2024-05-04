efd88a8155fd525bcfc0f0401f7ed16d8ca3060d
Koval Yaroslav <yaroslav@koval.page>
2024-05-04 21:10:15 +0200

    Update Telegram

--- src/elements/home/Telegram.svelte
+++ src/elements/home/Telegram.svelte
@@ -22,17 +22,12 @@
 </script>
 
 <div class="relative z-10 block px-8 mt-5" style="opacity: {opacity}">
-    <div class="w-full">
-        <script 
-            async 
-            src="https://telegram.org/js/telegram-widget.js?22" 
-            data-telegram-post={post} 
-            data-width="100%" 
-            data-color="343638" 
-            data-dark-color="FFFFFF"
-        ></script>
-    </div>
+    <iframe 
+        class="w-full mt-2 md:mt-3" 
+        src="https://t.me/{post}?embed=1&mode=tme" 
+        title="Telegram post"
+    ></iframe>
     <div class="block">
-        <NotButton onClick={handleJoin} className="m-auto mt-2 md:mt-4">Join</NotButton>
+        <NotButton onClick={handleJoin} className="m-auto">Join</NotButton>
     </div>
 </div>
